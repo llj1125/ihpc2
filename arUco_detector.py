2699b4d8034da0092a8810da8c61a993231d8174
changethisname2 <61044612+changethisname2@users.noreply.github.com>
2022-01-10 15:35:10 +0800

    Update arUco_detector.py

--- arUco_detector.py
+++ arUco_detector.py
@@ -26,13 +26,23 @@ out = cv2.aruco.drawDetectedMarkers(image, corners, ids)
 
 markerCentres = []
 
+count = 0
+
 for marker in corners:
     centreX = (marker[0][0][0] + marker[0][2][0]) / 2
     centreY = (marker[0][0][1] + marker[0][2][1]) / 2
-    markerTup = (centreX, centreY)
+    markerTup = (centreX, centreY, ids[count])
     markerCentres.append(markerTup)
+    count += 1
 
 print(markerCentres)
+
+sampleID = 1
+
+for marker in markerCentres:
+    if marker[2] == sampleID:
+        print(marker)
+
 
 cv2.imshow("out",out)
 cv2.waitKey(0)
